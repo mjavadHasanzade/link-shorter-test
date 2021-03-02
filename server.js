e1ff5107067c94032351b6b730c009e6c303db76
mjavadHasanzade <hjavad522@gmail.com>
2021-03-02 15:51:55 +0330

    improve view

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('app is listening to ' + process.env.PORT);
