8cbb247ef7c176c721fc1a76f8199b9060f18a08
mjavadHasanzade <hjavad522@gmail.com>
2021-03-02 15:57:02 +0330

    db connect

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shortUrl', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log('db connect');
+    }
+})
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/shortGenerator', (req, res) => {
     res.render('index');
 });
 
