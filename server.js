084d0db62b29338936c0859df5966ff7f736f951
mjavadHasanzade <hjavad522@gmail.com>
2021-03-02 20:31:25 +0330

    url shorter get url req

--- server.js
+++ server.js
@@ -29,6 +29,16 @@ app.post('/shortGenerator', async (req, res) => {
 
 });
 
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('app is listening to ' + process.env.PORT);
 })
